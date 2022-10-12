13714d9041b5c8498a5ef143dd59afb76c928773
noea7 <114760692+noea7@users.noreply.github.com>
2022-10-12 23:02:13 +0300

    Assignment 9 completed

---
+++ Pratybos9/src/lt/techin/kristina/Pratybos9/Pratybos9.java
@@ -0,0 +1,32 @@
+package lt.techin.kristina.Pratybos9;
+
+public class Pratybos9 {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Java");
+        System.out.println("Learning Java Now");
+        System.out.println("Programming is fun");
+        System.out.println("Crazy Java...");
+        System.out.println("Crazy Java...");
+        System.out.println("Crazy Java...");
+        System.out.println("Crazy Java...");
+        System.out.println("Crazy Java...");
+
+
+        System.out.println(" J      A    V     V     A");
+        System.out.println(" J     A A    V   V     A A");
+        System.out.println(" J    A A A    V V     A A A");
+        System.out.println("JJ   A     A    V     A     A");
+
+        int a = 1;
+        int b = 2;
+        int c = 3;
+        int d = 4;
+        System.out.println("___________________");
+        System.out.println("a   a^2   a^3   a^4");
+        System.out.println("___________________");
+        System.out.println(a + "   " + (int)Math.pow(a,2) + "     " + (int)Math.pow(a,3) + "     " + (int)Math.pow(a,4));
+        System.out.println(b + "   " + (int)Math.pow(b,2) + "     " + (int)Math.pow(b,3) + "     " + (int)Math.pow(b,4));
+        System.out.println(c + "   " + (int)Math.pow(c,2) + "     " + (int)Math.pow(c,3) + "    " + (int)Math.pow(c,4));
+        System.out.println(d + "   " + (int)Math.pow(d,2) + "    " + (int)Math.pow(d,3) + "    " + (int)Math.pow(d,4));
+    }
+}
