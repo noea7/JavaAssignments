7c4180e9d95e1c610b5743552c5e88e0a96c4487
noea7 <114760692+noea7@users.noreply.github.com>
2022-10-17 13:32:39 +0300

    Improved assignment 13.2 solution

--- Pratybos13/src/lt/techin/kristina/Pratybos13/Pratybos13_2.java
+++ Pratybos13/src/lt/techin/kristina/Pratybos13/Pratybos13_2.java
@@ -8,26 +8,27 @@ public class Pratybos13_2 {
         System.out.printf("Vidutinė sausio temperatūra: %.2f \n", averageData(dataJanuary));
         System.out.printf("Vidutinė vasario temperatūra: %.2f \n", averageData(dataFebruary));
         System.out.printf("Vidutinė kovo temperatūra: %.2f \n", averageData(dataMarch));
-        System.out.printf("Bendras atliktų matavimų vidurkis: %.2f \n", averageData(compoundData(dataJanuary, dataFebruary, dataMarch)));
+//        System.out.printf("Bendras atliktų matavimų vidurkis: %.2f \n", averageData(compoundData(dataJanuary, dataFebruary, dataMarch)));
+        System.out.printf("Bendras atliktų matavimų vidurkis: %.2f \n", averageData(dataJanuary,dataFebruary,dataMarch));
     }
 
-    private static double[] compoundData(double[] data1, double[] data2, double[] data3) {
-        double[] compoundData = new double[data1.length + data2.length + data3.length];
-        int index = 0;
-        for (int i = 0; i < data1.length; i++) {
-            compoundData[index] = data1[i];
-            index++;
-        }
-        for (int i = 0; i < data2.length; i++) {
-            compoundData[index] = data2[i];
-            index++;
-        }
-        for (int i = 0; i < data3.length; i++) {
-            compoundData[index] = data3[i];
-            index++;
-        }
-        return compoundData;
-    }
+//    private static double[] compoundData(double[] data1, double[] data2, double[] data3) {
+//        double[] compoundData = new double[data1.length + data2.length + data3.length];
+//        int index = 0;
+//        for (int i = 0; i < data1.length; i++) {
+//            compoundData[index] = data1[i];
+//            index++;
+//        }
+//        for (int i = 0; i < data2.length; i++) {
+//            compoundData[index] = data2[i];
+//            index++;
+//        }
+//        for (int i = 0; i < data3.length; i++) {
+//            compoundData[index] = data3[i];
+//            index++;
+//        }
+//        return compoundData;
+//    }
 
 
     private static double averageData(double ... data) {
@@ -36,5 +37,17 @@ public class Pratybos13_2 {
             sum += data[i];
         }
         return sum/data.length;
+    }
+
+    private static double averageData(double[] ... data) {
+        double sum = 0;
+        double length = 0;
+        for (double[] dataSet : data) {
+            for (int i = 0; i < dataSet.length; i++) {
+                sum += dataSet[i];
+            }
+            length+= dataSet.length;
+        }
+        return sum/length;
     }
 }
